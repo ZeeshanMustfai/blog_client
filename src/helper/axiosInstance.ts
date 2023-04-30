ccd9ac0b48282e0a1d38374ded9ae054b4b7d2b8
Zeeshan Mustfai <zeeshanmustfai22@gmail.com>
2023-04-30 18:08:28 +0500

    add axios instance

---
+++ src/helper/axiosInstance.ts
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+    }
+})
+
+axiosInstance.interceptors.request.use((req) => {
+  const user = localStorage.getItem('user') as string
+  const userData = JSON.parse(user)
+  if(userData){
+      req.headers.Authorization = `Bearer ${userData?.token}`;
+  }
+  return req;
+})
--- src/services/post-service.ts
+++ src/services/post-service.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { axiosInstance } from '../helper/axiosInstance'
 
 const API_URL = import.meta.env.VITE_API_URL
 
@@ -13,12 +14,12 @@ const response = await axios.post(`${API_URL}/upload`, data)
 return response.data
 }
 export const create_new_post = async (postData: TNewPost) => {
-  const response = await axios.post(`${API_URL}/post`, postData)
+  const response = await axiosInstance.post(`${API_URL}/post`, postData)
   return response.data
 }
 
 export const get_all_posts = async (id: string | undefined) => {
-    const response = await axios.get(`${API_URL}/post/all`, {
+    const response = await axiosInstance.get(`${API_URL}/post/all`, {
       params: {
         id
       }
@@ -27,16 +28,16 @@ export const get_all_posts = async (id: string | undefined) => {
 }
 
 export const get_one_post = async (id: string | undefined) => {
-  const response = await axios.get(`${API_URL}/post/${id}`)
+  const response = await axiosInstance.get(`${API_URL}/post/${id}`)
   return response.data
 }
 
 export const delete_one_post = async(id: string | undefined) => {
-  const response = await axios.delete(`${API_URL}/post/${id}`)
+  const response = await axiosInstance.delete(`${API_URL}/post/${id}`)
   return response.data
 }
 
 export const update_one_post = async(id: string | undefined, formData: TNewPost) => {
-  const response = await axios.put(`${API_URL}/post/${id}`, formData)
+  const response = await axiosInstance.put(`${API_URL}/post/${id}`, formData)
   return response.data
 }
