ed204a467ad19ccabf71af51656e4085a8166343
ZeeshanMustfai <zeeshanmustfai22@gmail.com>
2024-03-05 13:13:35 +0500

    add home page

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -7,42 +7,43 @@ import AddComment from './AddComment'
 const IMAGE_URL = import.meta.env.VITE_PF
 
 const PostDetail = () => {
-	const { id } = useParams()
-	const [postDetail, setPostDetail] = useState<TOnePost>()
-	const [isCommentAdded, setIsCommentAdd] = useState<boolean>()
+  const { id } = useParams()
+  const [postDetail, setPostDetail] = useState<TOnePost>()
+  const [isCommentAdded, setIsCommentAdd] = useState<boolean>()
 
-	const getPostComment = useCallback(() => {
-		setIsCommentAdd(!isCommentAdded)
-	}, [isCommentAdded])
-	const getDetailPost = async (postId: string | undefined) => {
-		const res = await get_one_post(postId)
-		setPostDetail(res)
-	}
+  const getPostComment = useCallback(() => {
+    setIsCommentAdd(!isCommentAdded)
+  }, [isCommentAdded])
 
-	useEffect(() => {
-		getDetailPost(id)
-	}, [id, isCommentAdded])
+  const getDetailPost = async (postId: string | undefined) => {
+    const res = await get_one_post(postId)
+    setPostDetail(res)
+  }
 
-	return (
-		<Container md>
-			<div>
-				<img
-					src={`${IMAGE_URL}${postDetail?.photo}`}
-					alt={postDetail?.title}
-					className={'detailImage'}
-				/>
-				<Spacer />
-				<Text size={'$4xl'} color='secondary'>
-					{postDetail?.title}
-				</Text>
-				<Text>{postDetail?.description}</Text>
-			</div>
-			<AddComment
-				commentList={postDetail?.comments}
-				getPostComment={getPostComment}
-			/>
-		</Container>
-	)
+  useEffect(() => {
+    getDetailPost(id)
+  }, [id, isCommentAdded])
+
+  return (
+    <Container md>
+      <div>
+        <img
+          src={`${IMAGE_URL}${postDetail?.photo}`}
+          alt={postDetail?.title}
+          className={'detailImage'}
+        />
+        <Spacer />
+        <Text size={'$4xl'} color='secondary'>
+          {postDetail?.title}
+        </Text>
+        <Text>{postDetail?.description}</Text>
+      </div>
+      <AddComment
+        commentList={postDetail?.comments}
+        getPostComment={getPostComment}
+      />
+    </Container>
+  )
 }
 
 export default PostDetail
--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -4,47 +4,48 @@ import { Link } from 'react-router-dom'
 const IMAGE_URL = import.meta.env.VITE_PF
 
 const PostPreview = ({ postData, handleDeletePost }: TPostPreview) => {
-	return (
-		<Card>
-			<Card.Body css={{ p: 0, m: 0 }}>
-				<Card.Image
-					objectFit='cover'
-					src={`${IMAGE_URL}${postData.photo}`}
-					width={'100%'}
-					height={200}
-				/>
-			</Card.Body>
-			<Card.Footer
-				css={{
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'flex-start',
-				}}
-			>
-				<Text css={{ fontSize: '$2xl' }}>{postData.title}</Text>
-				<div className='datePreview'>
-					<Text>{postData.description}</Text>
-				</div>
-				<div className='blogAction'>
-					<Link to={`/detail-post/${postData._id}`}>
-						<Button auto>Read more</Button>
-					</Link>
-					<Link to={`/create-post/${postData._id}`}>
-						<Button auto color='warning'>
-							Update
-						</Button>
-					</Link>
-					<Button
-						auto
-						color={'error'}
-						onPress={() => handleDeletePost(postData?._id)}
-					>
-						Delete
-					</Button>
-				</div>
-			</Card.Footer>
-		</Card>
-	)
+  console.log('postdata', postData)
+  return (
+    <Card>
+      <Card.Body css={{ p: 0, m: 0 }}>
+        <Card.Image
+          objectFit='cover'
+          src={`${IMAGE_URL}${postData.photo}`}
+          width={'100%'}
+          height={200}
+        />
+      </Card.Body>
+      <Card.Footer
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+        }}
+      >
+        <Text css={{ fontSize: '$2xl' }}>{postData.title}</Text>
+        <div className='datePreview'>
+          <Text>{postData.description}</Text>
+        </div>
+        <div className='blogAction'>
+          <Link to={`/detail-post/${postData._id}`}>
+            <Button auto>Read more</Button>
+          </Link>
+          <Link to={`/create-post/${postData._id}`}>
+            <Button auto color='warning'>
+              Update
+            </Button>
+          </Link>
+          <Button
+            auto
+            color={'error'}
+            onPress={() => handleDeletePost(postData?._id)}
+          >
+            Delete
+          </Button>
+        </div>
+      </Card.Footer>
+    </Card>
+  )
 }
 
 export default PostPreview
